25acc0dd680bee46f0624c986413809e921dab40
anusha-2024-aia <anusha2024aia@gmail.com>
2026-09-21 20:44:24 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,5 @@
+const {generateRandomNumber, celciusToFahrenheit} = require('./utils');
+
+console.log(`Random Number: ${generateRandomNumber()}`);
+
+console.log(`Temperature in Fahrenheit: ${celciusToFahrenheit(30)}`);
---
+++ utils.js
@@ -0,0 +1,10 @@
+function generateRandomNumber(min, max) {
+    return Math.floor(Math.random() * 100) + 1;
+}
+function celciusToFahrenheit(celsius) {
+    return (celsius * 9) / 5 + 32;
+}
+module.exports = {
+    generateRandomNumber,
+    celciusToFahrenheit
+};
