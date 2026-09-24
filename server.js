1cd416bbd560b940b20dd6bc1b049166d050069a
anusha-2024-aia <anusha2024aia@gmail.com>
2026-09-24 22:59:26 +0530

    Add project files

--- server.js
+++ server.js
@@ -1,27 +1,52 @@
 import http from 'http';
+import fs from 'fs/promises';
+import url from 'url';
+import path from 'path';
+
 
 const PORT = process.env.PORT || 8000;
 
-const server = http.createServer((req, res) => {
+//get current path
+
+const __filename = url.fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+console.log(__filename, __dirname);
+
+const server = http.createServer(async(req, res) => {
   try {
     if (req.method !== 'GET') {
-      throw new Error('Method not allowed');
+      let filePath;
+      // throw new Error('Method not allowed');
     }
 
     if (req.url === '/') {
-      res.writeHead(200, { 'Content-Type': 'text/html' });
-      res.end('<h1>Home Page</h1>');
-      return;
+      filePath = path.join(__dirname, 'public', 'index.html');
+      // res.writeHead(200, { 'Content-Type': 'text/html' });
+      // res.end('<h1>Home Page</h1>');
+      // return;
+    } else if (req.url === '/about') {
+      filePath = path.join(__dirname, 'public', 'about.html');
+      // res.writeHead(200, { 'Content-Type': 'text/html' });
+      // res.end('<h1>About Page</h1>');
+      // return;
+    } else {
+      throw new Error('Not Found');
     }
 
-    if (req.url === '/about') {
-      res.writeHead(200, { 'Content-Type': 'text/html' });
-      res.end('<h1>About Page</h1>');
-      return;
-    }
+    const data = await fs.readFile(filePath);
+    res.setHeader('Content-Type', 'text/html');
+    res.write(data);
+    res.end();
 
-    res.writeHead(404, { 'Content-Type': 'text/html' });
-    res.end('<h1>Not Found</h1>');
+    // if (req.url === '/about') {
+    //   res.writeHead(200, { 'Content-Type': 'text/html' });
+    //   res.end('<h1>About Page</h1>');
+    //   return;
+    // }
+
+    // res.writeHead(404, { 'Content-Type': 'text/html' });
+    // res.end('<h1>Not Found</h1>');
   } catch (error) {
     res.writeHead(500, { 'Content-Type': 'text/plain' });
     res.end('Internal Server Error');
